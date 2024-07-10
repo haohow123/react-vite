11148cce0746575106675c60a9d79223f0ef1c44
Hao <haohsu0219@gmail.com>
2024-07-10 19:26:27 +0800

    feat(global): add global layout and loading components

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { LinearProgress } from '@mui/material';
+
+function Loading() {
+  return (
+    <>
+      <LinearProgress sx={{ mx: -3, mt: -3 }} />
+    </>
+  );
+}
+
+export default Loading;
